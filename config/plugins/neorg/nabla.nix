aa5c18fa4407876dcf9444fba2f1407b8d5f1e64
John Szaniszlo <jjszaniszlo@poseidon.local>
2024-09-09 11:54:32 -0700

    fix nabla.nix

--- config/plugins/neorg/nabla.nix
+++ config/plugins/neorg/nabla.nix
@@ -6,7 +6,7 @@
           owner = "jbyuki";
           repo = "nabla.nvim";
           rev = "27a6ea9ed7452bb9e0b19eb0942b5bcf7d051b2f";
-          hash = pkgs.lib.fakeSha256;
+          sha256 = "1dnd6s6abwxihliqqyl1vrds14w502m0x7aadlxrrfh970bqikg0";
       };
     })
   ];
